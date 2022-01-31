fbb7d24cacb6906aee8543f1b800fa8fc7296769
saimaAnis21 <saima_rehman21@yahoo.com>
2022-01-31 17:20:13 +0500

    Add attributes to prod desc componenet

--- src/components/prodDesc.js
+++ src/components/prodDesc.js
@@ -38,9 +38,10 @@ class ProdDesc extends Component {
  
  
   render() {
-    const { prodData, bigImgSrc } = this.state;
-    console.log(prodData.attributes);
+    const { prodData, bigImgSrc } = this.state; 
+    let items = [];   
     if(this._isMounted){
+      console.log(prodData.attributes);
       return(
         <div style={{ border:'5px solid red', marginTop:'100px', display:'flex'}}>
           <div style={{ display:'flex', flexDirection:'column'}}>{ prodData.gallery.map( (pic) => (
@@ -50,7 +51,11 @@ class ProdDesc extends Component {
           <div>
             <div style={{ fontWeight:'600', fontSize:'30px', color:'#1D1F22'}}>{prodData.brand}</div>
             <div style={{ fontWeight:'normal', fontSize:'30px', color:'#1D1F22'}}>{prodData.name}</div>
-            
+            <div>{ prodData.attributes.length > 0 ? prodData.attributes.map((att) => (
+              <>
+              <p>{att.name}</p>
+               { att.name == "Color" ? att.items.map( item => <div style={{ color:`${item.value}`, backgroundColor:`${item.value}`, marginRight:'10px', textAlign:'center', display:'inline-block', border:'2px solid black'}}>{item.id}</div>) : att.items.map( item => <div style={{ marginRight:'10px', textAlign:'center', display:'inline-block', border:'2px solid black'}}>{item.id}</div>) }
+              </>)) : "no"} </div>
             <div style={{ fontWeight:'bold', fontSize:'18px', color:'#1D1F22', textTransform:'uppercase'}}>price:</div>
             <div>{ `${prodData.prices.find( (x) => x.currency.symbol == this.context.currencyLabel).amount} ${this.context.currencyLabel} `}</div>
             <div><Markup content={prodData.description} /></div>
